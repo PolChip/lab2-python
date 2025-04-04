e84db9595566a0f5cd04272305571434e8adeac2
PolChip <polinakraszx@gmail.com>
2025-04-05 00:24:37 +0300

    Add files via upload

---
+++ lab 2/lab2 nom3.py
@@ -0,0 +1,7 @@
+# Импортируем модуль
+import math_operations as m
+# Проверяем функции
+print(m.add(6, 10), 6+10)
+print(m.subtract(10, 6), 10-6)
+print(m.multiply(4, 6), 4*6)
+print(m.divide(11, 6), 10/6)
