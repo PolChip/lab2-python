02da727f641d9d1decc181ea615ff4218a225695
PolChip <polinakraszx@gmail.com>
2025-04-05 13:52:38 +0300

    Update lab 2 nom 1.py

--- lab 2/lab 2 nom 1.py
+++ lab 2/lab 2 nom 1.py
@@ -1,10 +1,10 @@
 # Читаем числа из файла data.txt построчно
 f = open('data.txt')
-mas = [int(f.readline().strip()) for i in range(10)]
-# Создаем новый файл, в который запишем результы
-ans = open('result.txt', 'w')
+b = [int(f.readline().strip()) for i in range(10)]
+# Создаем новый файл и запишем туда результы
+result = open('result.txt', 'w')
 # Построчно записываем результы
-ans.writelines('Сумма: ' + str(sum(mas)) + '\n')
-ans.writelines('Среднее: ' + str(sum(mas)//len(mas)) + '\n')
-ans.writelines('Максимум: ' + str(max(mas)) + '\n')
-ans.writelines('Минимум: ' + str(min(mas)) + '\n')
+result.writelines('Сумма: ' + str(sum(b)) + '\n')
+result.writelines('Среднее: ' + str(sum(b)//len(b)) + '\n')
+result.writelines('Максимум: ' + str(max(b)) + '\n')
+result.writelines('Минимум: ' + str(min(b)) + '\n')
